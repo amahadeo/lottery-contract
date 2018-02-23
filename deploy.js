d7159c4a4aa0b24211627698849e731deefed9c7
Anthony Mahadeo <anthony@financialapps.com>
2018-02-23 16:28:45 -0500

    update deploy script

--- deploy.js
+++ deploy.js
@@ -14,12 +14,11 @@ const deploy = async () => { // designed inside of function to be able to use as
 
   console.log('Attempting to deploy from account ', accounts[0]);
 
-  const INITIAL_STRING = 'Hi there!';
-
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+    console.log(interface);
     console.log('Contract deployed to', result.options.address);
 };
 deploy();
